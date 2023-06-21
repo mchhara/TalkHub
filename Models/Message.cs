ffd6197e9886fd49ed559bf5de412708d2d8b8fb
micha <harasim1999@gmail.pl>
2023-06-21 14:00:38 +0200

    THB-1

---
+++ Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalkHub.Models
+{
+	public class Message
+	{
+		public int Id { get; set; }
+		[Required]
+		public string UserName { get; set; }
+		[Required]
+		public string Text { get; set; }
+		public DateTime When { get; set; }
+		public int UserId { get; set; }
+		public virtual AppUser AppUser { get; set; }	
+	}
+}
