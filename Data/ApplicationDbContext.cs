b06d430977f4a8a2f1617222bc1dcfe52e752321
micha <harasim1999@gmail.pl>
2023-06-21 14:29:14 +0200

    THB-2

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TalkHub.Models;
 
 namespace TalkHub.Data
 {
 	public class ApplicationDbContext : IdentityDbContext
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<Message>()
+				.HasOne<AppUser>(a => a.Sender)
+				.WithMany(d => d.Messages)
+				.HasForeignKey(d => d.UserId);
 		}
 	}
 }
